ef823394d6bc182d56ef376e170fe1c3a4cb91df
AliZeeshan11 <147739161+AliZeeshan11@users.noreply.github.com>
2023-11-23 23:27:14 +0500

    Add files via upload

---
+++ Question 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter an integer: ";
+	cin>>num;
+	if(num>10 && num<=20)
+	{
+		cout<<"1";
+	} else 
+	{
+		cout<<"0";
+	}
+	return 0;
+} 
+
+
